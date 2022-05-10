5db0e21f45a77c5cd7da7258931000b2bef2cc7d
Jonathan Conrad <jc@Jonathans-MBP.home>
2022-05-10 13:36:26 -0700

    converted all worksheets into functions

--- fibonacci.py
+++ fibonacci.py
@@ -2,19 +2,21 @@
 # i.e. 1, 1, 2, 3, 5, 8, etc
 # write a method that does the Fibonacci sequence starting at 1
 
-get_val = input("Enter a number to start Fibonacci sequence: ")
-num_value = [0]
-for sequence in get_val:
-    num_value.append(int(sequence))
+# Fibonacci
 
-limiter = 0
+def run_fibonacci():
+    get_val = input("Enter a number to start Fibonacci sequence: ")
+    num_value = [0]
+    limiter = 0
+    for sequence in get_val:
+        num_value.append(int(sequence))
+    while limiter < 6:
+        incr_val = num_value[0] + num_value[1]
+        print(incr_val)
+        up_val = incr_val + num_value[0]
+        print(up_val)
+        num_value = [up_val]
+        num_value.append(int(incr_val))
+        limiter = limiter +1
 
-while limiter < 6:
-    incr_val = num_value[0] + num_value[1]
-    print(incr_val)
-    up_val = incr_val + num_value[0]
-    print(up_val)
-    num_value = [up_val]
-    num_value.append(int(incr_val))
-    limiter = limiter +1
-
+run_fibonacci()
--- worksheet2.py
+++ worksheet2.py
@@ -17,29 +17,29 @@
 
 # could use index values for pulling int values from an input("Enter a number to check if it's happy or sad: ")
 
-given_value = input("Enter any number, to check if it's happy or sad: ")
-num_value = []
-for new_func in given_value:
-    num_value.append(int(new_func))
 
-new_list = []
+# Happy Numbers
 
-while int(given_value) > 0:
-    new_value = pow(num_value[0], 2) + pow(num_value[1], 2)
-    if new_value in new_list:
-        print(f"Sorry! {given_value} is a sad number")
-        break
-    elif new_value > 1:
-        num_value = []
-        new_list.append(int(new_value))
-        for ints in str(new_value):
-            num_value.append(int(ints))
-    elif new_value == 1:
-        print(f"Great! {given_value} is a happy number!")
-        break
-    else:
-        break
+def is_num_happy():
+    given_value = input("Enter any number, to check if it's happy or sad: ")
+    num_value = []
+    for new_func in given_value:
+        num_value.append(int(new_func))
+    new_list = []
+    while int(given_value) > 0:
+        new_value = pow(num_value[0], 2) + pow(num_value[1], 2)
+        if new_value in new_list:
+            print(f"Sorry! {given_value} is a sad number")
+            break
+        elif new_value > 1:
+            num_value = []
+            new_list.append(int(new_value))
+            for ints in str(new_value):
+                num_value.append(int(ints))
+        elif new_value == 1:
+            print(f"Great! {given_value} is a happy number!")
+            break
+        else:
+            break
 
-
-
-
+is_num_happy()
